525071a8519732e667bca3b0cb72a8f5b53d1728
braguru <bra_guru@protonmail.com>
2023-08-19 09:45:47 +0100

    Added informational pages

--- CCFraud/CCFraud/settings.py
+++ CCFraud/CCFraud/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -31,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'core',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -115,7 +117,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'static/css/'
+MEDIA_URL = '/images/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'core/static')
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- CCFraud/core/views.py
+++ CCFraud/core/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.views.generic import ListView, TemplateView
 
 # Create your views here.
+
+class Landing_Page(TemplateView):
+    template_name = "core/landing_page.html"
+    
+    
+class About(TemplateView):
+    template_name = "core/About.html"
+    
